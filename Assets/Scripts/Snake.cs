26b7e26553c7901a3e704f2d520259db4ebdd617
MatChumski <mateo0704@hotmail.com>
2022-08-20 14:24:15 -0500

    Update 20/08/2022
    Se añade la pantalla de game over
    Aleatorización de la generación del tablero

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -2,26 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Snake : MonoBehaviour
 {
 
     // Definir los límites del tamaño del entorno de juego
     public int xSize, ySize;
+
+    public Camera camara;
+
     public AudioSource soundEffect;
     public AudioClip deathAudio;
     public AudioClip eatAudio;
 
+    // Bloque base
     public GameObject block;
 
-
+    // Cabeza y dirección
     GameObject head;
     string headDirection = "right";
 
+    // Cuerpo 
     List<GameObject> tail;
 
+    // Comida
     GameObject food;
 
+    // Materiales
     public Material headMaterial;
     public Material tailMaterial;
     public Material wallMaterial;
@@ -30,26 +38,45 @@ public class Snake : MonoBehaviour
     public Material deadHeadMaterial;
     public Material backgroundMaterial;
 
+    // Velocidad a la que la serpiente va a cambiar de posición
     public float gameSpeed = 0.7f;
 
-    public Text scoreText;
+    // Interfaz
+    // InGame
+    public GameObject UI;
+    public Text scoreText;  // Puntaje
     public float score = 0;
-
-    public Text speedText;
+    public Text speedText; // Velocidad
+    // GameOver
+    public GameObject gameOver;
+    public Button retry;
 
     // Start is called before the first frame update
     void Start()
     {
+        xSize = GenerateEven(10, 30);
+        ySize = GenerateEven(10, 30);
+
         CreateGrid();
         CreatePlayer();
 
+        camara.GetComponent<Camera>().orthographicSize = Mathf.Max(xSize, ySize)/2 + 1;
+        
+        // Oculta el mensaje de Game Over
+        gameOver.SetActive(false);
+
+        // Establece la acción para el botón de restart
+        retry.onClick.AddListener(RestartGame);
+
+        // Crea la única instancia de comida que se va a mover
         food = Instantiate(block);
         food.GetComponent<MeshRenderer>().material = foodMaterial;
         MoveFood();
 
         UpdateSpeed();
 
-        InvokeRepeating("MoveHead", 0, gameSpeed); //calls moveHead() every 2 secs
+        // Hace que la función MoveHead() se llame en intervalos según gameSpeed
+        InvokeRepeating("MoveHead", 0, gameSpeed);
     }
 
     // Update is called once per frame
@@ -61,13 +88,14 @@ public class Snake : MonoBehaviour
         float foodPositionX = food.GetComponent<Transform>().position.x;
         float foodPositionY = food.GetComponent<Transform>().position.y;
 
-
+        // Si la cabeza está fuera de los límites, muere el jugador
         if (headPositionX >= xSize / 2 || headPositionX <= -xSize / 2 || headPositionY >= ySize / 2 || headPositionY <= -ySize / 2)
         {
             KillPlayer();
         }
 
-        foreach(GameObject part in tail)
+        // Si la cabeza toca alguna parte de su cola, muere el jugador
+        foreach (GameObject part in tail)
         {
             if (headPositionX == part.GetComponent<Transform>().position.x && headPositionY == part.GetComponent<Transform>().position.y)
             {
@@ -76,36 +104,59 @@ public class Snake : MonoBehaviour
             }
         }
 
+        // Si la posición de la cabeza coincide con la comida, mueve la comida y aumenta el tamaño
         if (headPositionX == foodPositionX && headPositionY == foodPositionY)
         {
             MoveFood();
             EatFood();
         }
 
+        /* 
+        Cambiar de dirección
+        Si no tiene cola, puede moverse en cualquier dirección, pero si la tiene, no puede ir
+        en la dirección contraria a la actual
+        */
+
         if (Input.GetKeyDown("down"))
         {
-            if (headDirection != "up")
+            if (tail.Count > 0 && headDirection != "up")
+            {
+                headDirection = "down";
+            }
+            else if (tail.Count <= 0)
             {
                 headDirection = "down";
             }
         }
         if (Input.GetKeyDown("up"))
-            if (headDirection != "down")
+        {
+            if (tail.Count > 0 && headDirection != "down")
             {
                 headDirection = "up";
             }
-        {
+            else if (tail.Count <= 0)
+            {
+                headDirection = "up";
+            }
         }
         if (Input.GetKeyDown("left"))
         {
-            if (headDirection != "right")
+            if (tail.Count > 0 && headDirection != "right")
+            {
+                headDirection = "left";
+            }
+            else if (tail.Count <= 0)
             {
                 headDirection = "left";
             }
         }
         if (Input.GetKeyDown("right"))
         {
-            if (headDirection != "left")
+            if (tail.Count > 0 && headDirection != "left")
+            {
+                headDirection = "right";
+            }
+            else if (tail.Count <= 0)
             {
                 headDirection = "right";
             }
@@ -113,6 +164,23 @@ public class Snake : MonoBehaviour
 
     }
 
+    private int GenerateEven(int min, int max)
+    {
+        bool isEven = false;
+        int num = 0;
+        while (!isEven)
+        {
+            num = Random.Range(min, max);
+            if (num % 2 == 0)
+            {
+                isEven = true;
+            }
+        }
+
+        return num;
+    }
+
+    // Crear Jugador
     private void CreatePlayer()
     {
         head = Instantiate(block);
@@ -120,10 +188,11 @@ public class Snake : MonoBehaviour
         head.GetComponent<Transform>().position = new Vector3(0, 0, 0);
         tail = new List<GameObject>();
     }
-
+    
+    // Matar Jugador
     private void KillPlayer()
     {
-        CancelInvoke();
+        CancelInvoke(); // Detiene el ciclo de movimiento
         head.GetComponent<MeshRenderer>().material = deadHeadMaterial;
 
         foreach (GameObject part in tail)
@@ -135,24 +204,37 @@ public class Snake : MonoBehaviour
         soundEffect.Play();
 
         Destroy(head);
+
+        // Muestra el mensaje de Game Over
+        gameOver.SetActive(true);
     }
 
+    // Reiniciar Juego
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+    // Actualizar puntaje en pantalla
     private void UpdateScore()
     {
         score += 1;
         scoreText.text = "Score: " + score.ToString();
     }
 
+    // Actualiza velocidad en pantalla
     private void UpdateSpeed()
     {
-        speedText.GetComponent<Text>().text = "Speed: " + Mathf.Round(1 / gameSpeed).ToString() + "m/s";
+        speedText.GetComponent<Text>().text = "Speed: " + System.Math.Round((1 / gameSpeed), 2).ToString() + "m/s";
     }
 
+    // Movimiento de la cabeza
     private void MoveHead()
     {
         float headPositionX = head.GetComponent<Transform>().position.x;
         float headPositionY = head.GetComponent<Transform>().position.y;
 
+        // Según la dirección actual, va a cambiar su posición
         if (headDirection == "right")
         {
             head.GetComponent<Transform>().position = new Vector3(headPositionX + 1, headPositionY, 0);
@@ -170,28 +252,39 @@ public class Snake : MonoBehaviour
             head.GetComponent<Transform>().position = new Vector3(headPositionX, headPositionY - 1, 0);
         }
 
+        // Tras mover la cabeza, mueve la cola. Envía la posición en la que estaba ANTES de moverse
         MoveTail(headPositionX, headPositionY);
     }
 
+    // Movimiento de la cola
     private void MoveTail(float lastX, float lastY)
     {
+        // Recorre la cola
         foreach (GameObject part in tail)
         {
+            // Establece como nueva posición la del bloque anterior
+            // Inicialmente, es la posición en la que estaba la cabeza previamente
             float newX = lastX;
             float newY = lastY;
 
+            // Establece como posición anterior la ubicación actual del bloque que se va a mover, para
+            // pasarla a la siguiente interación
             lastX = part.GetComponent<Transform>().position.x;
             lastY = part.GetComponent<Transform>().position.y;
 
+            // Mueve el bloque actual a la posición nueva, que corresponde a la del bloque anterior
             part.GetComponent<Transform>().position = new Vector3(newX, newY, 0);
         }
     }
 
+    // Mover Comida
     private void MoveFood()
     {
+        // Nuevas posiciones
         float newX;
         float newY;
 
+        // Genera una posición nueva aleatoria según un tamaño máximo
         static float RandomPosition(int maxSize)
         {
             float randomPos = Random.Range(maxSize / 2 - 1, -maxSize / 2 + 1);
@@ -199,14 +292,15 @@ public class Snake : MonoBehaviour
             return randomPos;
         }
 
-
+        // Genera las nuevas posiciones
         newX = RandomPosition(xSize);
         newY = RandomPosition(ySize);
 
+        // Mientras que la posición no sea válida, se va a repetir el ciclo
         bool valid = false;
         while (!valid)
         {
-            bool found = false;
+            bool found = false; // ¿Las posiciones chocan?
             foreach (GameObject part in tail)
             {
                 if (part.GetComponent<Transform>().position.x == newX && part.GetComponent<Transform>().position.y == newY)
@@ -215,26 +309,33 @@ public class Snake : MonoBehaviour
                     continue;
                 }
             }
+
+            // Si chocan, genera una nueva posición
             if (found)
             {
                 valid = false;
                 newX = RandomPosition(xSize);
                 newY = RandomPosition(ySize);
-            } else
+            }
+            else // Si no, es válido
             {
                 valid = true;
             }
-            
+
         }
 
+        // Cambia la posición de la comida
         food.GetComponent<Transform>().position = new Vector3(newX, newY, 0);
     }
 
+    // Comer
     private void EatFood()
     {
+        // Última posición de la cola
         float lastPositionX;
         float lastPositionY;
 
+        // Si no hay cola, guarda la posición de la cabeza
         if (tail.Count == 0)
         {
             lastPositionX = head.GetComponent<Transform>().position.x;
@@ -247,11 +348,14 @@ public class Snake : MonoBehaviour
 
         }
 
+        // Añade un elemento a la cola
         tail.Add(Instantiate(block));
         tail[tail.Count - 1].GetComponent<MeshRenderer>().material = tailMaterial;
 
         //tail[tail.Count - 1].GetComponent<Transform>().position = new Vector3(lastPositionX, lastPositionY, 0);
-
+                
+        // Según la dirección en la que esté yendo, añade este bloque a la posición anterior al último bloque de la cola,
+        // que se guardó anteriormente
         if (headDirection == "right")
         {
             tail[tail.Count - 1].GetComponent<Transform>().position = new Vector3(lastPositionX - 1, lastPositionY, 0);
@@ -268,12 +372,14 @@ public class Snake : MonoBehaviour
         {
             tail[tail.Count - 1].GetComponent<Transform>().position = new Vector3(lastPositionX, lastPositionY + 1, 0);
         }
-
+        
+        // Actualiza el puntaje
         UpdateScore();
 
-        if (score % 5 == 0 && gameSpeed > 0.11f)
+        // Si la el tiempo de actualización está por encima de 0.11 ms, aumenta la velocidad del juego (disminuye gameSpeed)
+        if (gameSpeed > 0.11f)
         {
-            gameSpeed -= 0.05f;
+            gameSpeed -= 0.01f;
             UpdateSpeed();
             CancelInvoke();
             InvokeRepeating("MoveHead", gameSpeed, gameSpeed);
@@ -292,22 +398,22 @@ public class Snake : MonoBehaviour
             // Se crea una instancia del objecto block para borderBottom
             GameObject borderBottom = Instantiate(block);
             borderBottom.GetComponent<MeshRenderer>().material = wallMaterial;
-            borderBottom.GetComponent<Transform>().position = new Vector3(x - (xSize / 2), -ySize / 2, 0.1f);
+            borderBottom.GetComponent<Transform>().position = new Vector3(x - (xSize / 2), -ySize / 2, -0.2f);
 
             GameObject borderTop = Instantiate(block);
             borderTop.GetComponent<MeshRenderer>().material = wallMaterial;
-            borderTop.GetComponent<Transform>().position = new Vector3(x - (xSize / 2), ySize / 2, 0.1f);
+            borderTop.GetComponent<Transform>().position = new Vector3(x - (xSize / 2), ySize / 2, -0.2f);
         }
 
         for (int y = 0; y <= ySize; y++)
         {
             GameObject borderLeft = Instantiate(block);
             borderLeft.GetComponent<MeshRenderer>().material = wallMaterial;
-            borderLeft.GetComponent<Transform>().position = new Vector3(-xSize / 2, y - (ySize / 2), 0.1f);
+            borderLeft.GetComponent<Transform>().position = new Vector3(-xSize / 2, y - (ySize / 2), -0.2f);
 
             GameObject borderRight = Instantiate(block);
             borderRight.GetComponent<MeshRenderer>().material = wallMaterial;
-            borderRight.GetComponent<Transform>().position = new Vector3(xSize / 2, y - (ySize / 2), 0.1f);
+            borderRight.GetComponent<Transform>().position = new Vector3(xSize / 2, y - (ySize / 2), -0.2f);
         }
 
         GameObject background = Instantiate(block);
